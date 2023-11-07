7bbd1ccf264a6ae1e6a64f68b1ba797cc0e936a2
Didac Soler <solerdidac>
2023-11-07 12:59:38 +0100

    Programa para subar en C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {      
+        double num1, num2, suma;
+
+        Console.Write("Ingresa el primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        suma = num1 + num2;
+
+        Console.WriteLine($"La suma de {num1} y {num2} es: {suma}");
+    }
+}
